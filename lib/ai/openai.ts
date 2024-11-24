ff066051b363e3d7cb58d946834a7417ad0b54ee
hxf666 <99327546+alanyagger@users.noreply.github.com>
2024-11-25 01:13:04 +0800

    Update openai.ts

--- lib/ai/openai.ts
+++ lib/ai/openai.ts
@@ -1,3 +1,5 @@
+import { createOpenAI } from '@ai-sdk/openai';
+
 const openai = createOpenAI({
   baseURL : 'https://api.chatanywhere.tech/v1',
   apiKey : 'sk-0Z8kkECGfmqum1aoTIKTHH8oVYqWnNGC3scdAZpSr8RnuvGs',
