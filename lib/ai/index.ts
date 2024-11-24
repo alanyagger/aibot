e19aeb4582f84728c77fea0d2fce0fb50f69e816
hxf666 <99327546+alanyagger@users.noreply.github.com>
2024-11-25 01:18:42 +0800

    Update index.ts

--- lib/ai/index.ts
+++ lib/ai/index.ts
@@ -1,7 +1,14 @@
 //import { openai } from '@ai-sdk/openai';
 import { experimental_wrapLanguageModel as wrapLanguageModel } from 'ai';
-import {openai} from './openai';
 import { customMiddleware } from './custom-middleware';
+import { createOpenAI } from '@ai-sdk/openai';
+
+const openai = createOpenAI({
+  baseURL : 'https://api.chatanywhere.tech/v1',
+  apiKEY : 'sk-0Z8kkECGfmqum1aoTIKTHH8oVYqWnNGC3scdAZpSr8RnuvGs',
+  compatibility: 'compatible',
+});
+
 
 export const customModel = (apiIdentifier: string) => {
   return wrapLanguageModel({
